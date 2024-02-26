78210574dfda9e2df37853e052550e1c1e4f7994
iconlynx <marcelinowojcik@gmail.com>
2024-02-26 11:32:37 +0100

    project(feat): Menu implementation and placeholder content

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import {useState} from "react";
 import {Loader} from "./components/loader.tsx";
+import {Header} from "./components/header.tsx";
 import {Content} from "./components/content.tsx";
 import Spline from '@splinetool/react-spline';
 import './App.sass';
+import {Menu} from "./components/menu.tsx";
 
 export default function App() {
   const [loading, setLoading] = useState(true);
@@ -10,8 +12,10 @@ export default function App() {
   return (
     <>
       <Loader loading={loading} />
+      <Menu />
       <section>
         <Spline onLoad={() => setLoading(false)} className="spline" scene="https://prod.spline.design/VlAd4WmOD1IJbEpJ/scene.splinecode" />
+        <Header />
         <Content />
       </section>
     </>
---
+++ src/components/header.tsx
@@ -0,0 +1,27 @@
+import "../styles/header.sass";
+import {TypeAnimation} from "react-type-animation";
+
+export function Header() {
+  return (
+    <div id={"header"}>
+      <h1>Portfolio</h1>
+      <h2>Marcel Wójcik</h2>
+      <p>
+        <TypeAnimation
+          sequence={[
+            "",
+            3000,
+            "Web Developer",
+            1000,
+            "Software Developer",
+            1000,
+            "UX/UI Designer",
+            1000
+          ]}
+          speed={50}
+          repeat={Infinity}
+        />
+      </p>
+    </div>
+  )
+}
